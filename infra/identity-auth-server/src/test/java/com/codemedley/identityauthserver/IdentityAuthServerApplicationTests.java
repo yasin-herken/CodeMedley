671e40f6e6b5d78af0a5c6935a944a8f074eff17
Yasin HERKEN <yasin.herken@tubitak.gov.tr>
2025-08-19 21:30:55 +0300

    CodeMedley-7 .

---
+++ infra/identity-auth-server/src/test/java/com/codemedley/identityauthserver/IdentityAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemedley.identityauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
