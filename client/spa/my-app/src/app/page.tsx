91e3b2229a35c15dce0c5a419d1bb8d2daa90162
Yasin HERKEN <yasin.herken@tubitak.gov.tr>
2025-08-19 21:11:40 +0300

    CodeMedley-6 some changes were made

--- client/spa/my-app/src/app/page.tsx
+++ client/spa/my-app/src/app/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import {useState} from 'react';
 
 interface Book {
@@ -9,7 +7,7 @@ interface Book {
 
 export default function BookListApp() {
     const [books, setBooks] = useState<Book[]>([]);
-    const [username, setUsername] = useState('');
+
 
     const loadBooks = async () => {
         try {
